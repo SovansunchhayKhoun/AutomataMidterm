be7c75161235709914c62f5adf5ef8a448dea737
RethtihpongEM <rithtipongem@gmail.com>
2023-07-19 23:40:19 +0700

    fix transition table

--- client/src/context/MinimizedContext.jsx
+++ client/src/context/MinimizedContext.jsx
@@ -4,17 +4,33 @@ export const MinimizedContext = createContext();
 export const MinimizedProvider = ({ children }) => {
   const [states, setStates] = useState([]);
   const [alphabets, setAlphabets] = useState([]);
-  const [transitionTable, setTransitionTable] = useState(Array.from({length: 1},()=> Array.from({length: 1}, () => 'q0')));
+  const [transitionTable, setTransitionTable] = useState(
+    Array.from({ length: 1 }, () => Array.from({ length: 1 }, () => "q0"))
+  );
 
-  const handleTransistionTable = (row,column,value) => {
+  const initializeTransitionTable = (row, column, value = "q0") => {
+    setTransitionTable(
+      Array.from({ length: row }, () =>
+        Array.from({ length: column }, () => value)
+      )
+    );
+    setDfa({
+      ...dfa,
+      transitions: Array.from({ length: row }, () =>
+        Array.from({ length: column }, () => value)
+      ),
+    });
+  };
+
+  const handleTransistionTable = (row, column, value) => {
     //temporary copy from transitionTable
     let copy = [...transitionTable];
     //change value
     copy[row][column] = value;
     //update transitionTable value
     setTransitionTable(copy);
-    setDfa({...dfa,transitions:transitionTable})
-  }
+    setDfa({ ...dfa, transitions: copy });
+  };
   //dfa components
   const [dfa, setDfa] = useState({
     states: states,
@@ -23,19 +39,19 @@ export const MinimizedProvider = ({ children }) => {
     finalStates: [],
     amountState: states.length,
     amountAlphabet: alphabets.length,
-    transitions: transitionTable
+    transitions: transitionTable,
   });
   const generateStates = (amountState = 1) => {
     //clear all states from the array
     while (states.length > 0) {
       states.pop();
-      setDfa({ ...dfa, states: states,amountState: states.length});
+      setDfa({ ...dfa, states: states, amountState: states.length });
     }
 
     //insert all states to array
     for (let i = 0; i < amountState; i++) {
       setStates(states, states.push(`q${i}`));
-      setDfa({ ...dfa, states: states,amountState: states.length});
+      setDfa({ ...dfa, states: states, amountState: states.length });
     }
   };
 
@@ -43,12 +59,20 @@ export const MinimizedProvider = ({ children }) => {
     //clear all alphabets from the array
     while (alphabets.length > 0) {
       alphabets.pop();
-      setDfa({ ...dfa, alphabets: alphabets,amountAlphabet: alphabets.length});
+      setDfa({
+        ...dfa,
+        alphabets: alphabets,
+        amountAlphabet: alphabets.length,
+      });
     }
     //insert all alphabets to array
     for (let i = 0; i < amountAlphabet; i++) {
       setAlphabets(alphabets, alphabets.push(`${i}`));
-      setDfa({ ...dfa, alphabets: alphabets,amountAlphabet: alphabets.length});
+      setDfa({
+        ...dfa,
+        alphabets: alphabets,
+        amountAlphabet: alphabets.length,
+      });
     }
   };
 
@@ -95,7 +119,8 @@ export const MinimizedProvider = ({ children }) => {
         handleFinalState,
         transitionTable,
         setTransitionTable,
-        handleTransistionTable
+        handleTransistionTable,
+        initializeTransitionTable,
       }}
     >
       {children}
--- client/src/views/MinimizedDfa.jsx
+++ client/src/views/MinimizedDfa.jsx
@@ -12,159 +12,159 @@ export const MinimizedDfa = () => {
     alphabets,
     handleStartState,
     handleFinalState,
-    setTransitionTable,
+    initializeTransitionTable,
   } = useContext(MinimizedContext);
   useEffect(() => {
-    generateAlphabets()
-    generateStates()
-  },[]);
-    return (
-      <main className="flex gap-4">
-        <section className="w-full">
-          <div className="grid grid-cols-2 gap-4 w-1/2 items-center">
-            {/* get number of states */}
-            <label htmlFor="state">Number of States: </label>
-            <div className="flex flex-col gap-1">
-              <input
-                min={1}
-                defaultValue={1}
-                type="number"
-                required
-                max={5}
-                onChange={(event) => {
-                  generateStates(event.target.value);
-                  setTransitionTable(Array.from({length: event.target.value},()=> Array.from({length: alphabets.length}, () => 'q0')))
-                }}
-                className="border-2 border-[#3B82F6] px-2 py-1 w-full"
-                id="state"
-              />
-              <div>
-                Your states: [{" "}
-                {states?.map((state, key) => {
-                  return (
-                    <span key={key}>
-                      {state}
-                      {states.length - 1 !== key && ", "}
-                    </span>
-                  );
-                })}{" "}
-                ]
-              </div>
-            </div>
-
-            {/* get number of alphabets */}
-            <label htmlFor="alphabet">Number of Alphabets: </label>
+    generateAlphabets();
+    generateStates();
+  }, []);
+  return (
+    <main className="flex gap-4">
+      <section className="w-full">
+        <div className="grid grid-cols-2 gap-4 w-1/2 items-center">
+          {/* get number of states */}
+          <label htmlFor="state">Number of States: </label>
+          <div className="flex flex-col gap-1">
+            <input
+              min={1}
+              defaultValue={1}
+              type="number"
+              required
+              max={5}
+              onChange={(event) => {
+                generateStates(event.target.value);
+                initializeTransitionTable(event.target.value, alphabets.length);
+              }}
+              className="border-2 border-[#3B82F6] px-2 py-1 w-full"
+              id="state"
+            />
             <div>
-              <input
-                required
-                min="1"
-                defaultValue="1"
-                onChange={(event) => {
-                  generateAlphabets(event.target.value);
-                  setTransitionTable(Array.from({length: states.length},()=> Array.from({length: event.target.value}, () => 'q0')))
-                }}
-                max="5"
-                className="border-2 border-[#3B82F6] px-2 py-1 w-full"
-                type="number"
-                id="alphabet"
-              />
-              <div>
-                Your Alphabets: [{" "}
-                {alphabets?.map((alphabet, key) => {
-                  return (
-                    <span key={key}>
-                      {" "}
-                      {alphabet}
-                      {alphabets.length - 1 !== key && ", "}
-                    </span>
-                  );
-                })}{" "}
-                ]
-              </div>
+              Your states: [{" "}
+              {states?.map((state, key) => {
+                return (
+                  <span key={key}>
+                    {state}
+                    {states.length - 1 !== key && ", "}
+                  </span>
+                );
+              })}{" "}
+              ]
             </div>
-
-            {/* get start state */}
-            {states.length > 0 && (
-              <>
-                <div>Start State</div>
-                <div>
-                  <div className="grid grid-cols-5 gap-1">
-                    {states?.map((state, key) => {
-                      return (
-                        <div key={key} className="flex gap-1 items-center">
-                          <input
-                            type="checkbox"
-                            onChange={(event) => {
-                              handleStartState(event);
-                            }}
-                            value={state}
-                            id={state}
-                          />
-                          <label htmlFor={state}>{state}</label>
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <div>
-                    Your Start State:{" "}
-                    {dfa.startState?.map((startState, key) => (
-                      <span key={key}>
-                        {startState}
-                        {dfa.startState.length - 1 !== key && ", "}
-                      </span>
-                    ))}
-                  </div>
-                </div>
-              </>
-            )}
-
-            {/* get final state */}
-            {states.length > 0 && (
-              <>
-                <div>Final State</div>
-                <div>
-                  <div className="grid grid-cols-5 gap-1">
-                    {states?.map((state, key) => {
-                      return (
-                        <div key={key} className="flex gap-1 items-center">
-                          <input
-                            type="checkbox"
-                            onChange={(event) => {
-                              handleFinalState(event);
-                            }}
-                            value={state}
-                            id={state}
-                          />
-                          <label htmlFor={state}>{state}</label>
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <div>
-                    Your Final State:{" "}
-                    {dfa.finalStates?.map((finalState, key) => (
-                      <span key={key}>
-                        {finalState}
-                        {dfa.finalStates.length - 1 !== key && ", "}
-                      </span>
-                    ))}
-                  </div>
-                </div>
-              </>
-            )}
-
-            <label htmlFor="">Transition Table</label>
-            <DfaTransitionTable />
           </div>
 
-          <button
-            onClick={() => {
-              console.log(dfa);
-            }}
-          >
-            Submit
-          </button>
-        </section>
-      </main>
-    );
+          {/* get number of alphabets */}
+          <label htmlFor="alphabet">Number of Alphabets: </label>
+          <div>
+            <input
+              required
+              min="1"
+              defaultValue="1"
+              onChange={(event) => {
+                generateAlphabets(event.target.value);
+                initializeTransitionTable(states.length, event.target.value);
+              }}
+              max="5"
+              className="border-2 border-[#3B82F6] px-2 py-1 w-full"
+              type="number"
+              id="alphabet"
+            />
+            <div>
+              Your Alphabets: [{" "}
+              {alphabets?.map((alphabet, key) => {
+                return (
+                  <span key={key}>
+                    {" "}
+                    {alphabet}
+                    {alphabets.length - 1 !== key && ", "}
+                  </span>
+                );
+              })}{" "}
+              ]
+            </div>
+          </div>
+
+          {/* get start state */}
+          {states.length > 0 && (
+            <>
+              <div>Start State</div>
+              <div>
+                <div className="grid grid-cols-5 gap-1">
+                  {states?.map((state, key) => {
+                    return (
+                      <div key={key} className="flex gap-1 items-center">
+                        <input
+                          type="checkbox"
+                          onChange={(event) => {
+                            handleStartState(event);
+                          }}
+                          value={state}
+                          id={state}
+                        />
+                        <label htmlFor={state}>{state}</label>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div>
+                  Your Start State:{" "}
+                  {dfa.startState?.map((startState, key) => (
+                    <span key={key}>
+                      {startState}
+                      {dfa.startState.length - 1 !== key && ", "}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
+
+          {/* get final state */}
+          {states.length > 0 && (
+            <>
+              <div>Final State</div>
+              <div>
+                <div className="grid grid-cols-5 gap-1">
+                  {states?.map((state, key) => {
+                    return (
+                      <div key={key} className="flex gap-1 items-center">
+                        <input
+                          type="checkbox"
+                          onChange={(event) => {
+                            handleFinalState(event);
+                          }}
+                          value={state}
+                          id={state}
+                        />
+                        <label htmlFor={state}>{state}</label>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div>
+                  Your Final State:{" "}
+                  {dfa.finalStates?.map((finalState, key) => (
+                    <span key={key}>
+                      {finalState}
+                      {dfa.finalStates.length - 1 !== key && ", "}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </>
+          )}
+
+          <label htmlFor="">Transition Table</label>
+          <DfaTransitionTable />
+        </div>
+
+        <button
+          onClick={() => {
+            console.log(dfa);
+          }}
+        >
+          Submit
+        </button>
+      </section>
+    </main>
+  );
 };
